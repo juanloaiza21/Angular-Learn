a44c28f77b41c1da540201f04f2b9819b24399ef
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-05-01 17:14:19 -0500

    class biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ export class AppComponent {
   name: string = 'Juan Loaiza';
   age: number = 18;
   img: string = "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080"
+  bntDisabled = true;
+  person = {
+      name: "Juan Loaiza",
+      age: 18,
+      avatar: "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080",
+  }
 }
