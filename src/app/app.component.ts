0296133c14fe90f5dc0761a20c512c016a851a0b
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-05-01 17:00:27 -0500

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  //Toda variable debe ser pública para poder ser usada en app.component.html.
+  name: string = 'Juan Loaiza';
+  age: number = 18;
+  img: string = "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080"
 }
