cd9667ec7654e7d9f52e55ce3d4100a560909d35
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-05-01 17:28:54 -0500

    event biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,22 @@ export class AppComponent {
   img: string = "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080"
   bntDisabled = true;
   person = {
-      name: "Juan Loaiza",
-      age: 18,
-      avatar: "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080",
+    name: "Juan Loaiza",
+    age: 18,
+    avatar: "https://images.unsplash.com/photo-1650223457799-93853451d7cd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTQ0MjMxMA&ixlib=rb-1.2.1&q=80&w=1080",
+  }
+
+  //Metodo
+
+  toggleButton(){
+    if (!this.bntDisabled) {
+      this.bntDisabled = true;
+    } else {
+      this.bntDisabled = false;
+    }
+  }
+
+  ageIncreaser(){
+    this.person.age += 1;
   }
 }
